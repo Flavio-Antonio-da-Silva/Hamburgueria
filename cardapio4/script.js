eda36d5c9f6f8756af729c40a054fd0af2729943
Flávio Antônio <flavusantonio@gmail.com>
2025-07-12 17:42:24 -0300

    Código rodando bem até aqui.
    12/07/2025 às 17:42 clock

--- cardapio4/script.js
+++ cardapio4/script.js
@@ -1,11 +1,39 @@
-const menu = getElementById("menu")
-const cartBtn = getElementById ("cart-btn")
-const cartModal = getElementBbyId ("cart-modal")
-const CartItems = getElementById ("cart-items")
-const cartTotal = getElementBbyId ("cart-total")
-const checkoutBtn = getElementBbyId ("checkout-btn")
-const closeBodalBtn = getElementBbyId ("close-modal-btn")
-const cartCount = getElementBbyId ("cart-count")
-const addressInput = getElementBbyId("address")
-const addressWarn = getElementBbyId ("address-warn") brasil
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItems = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCount = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
 
+//Abrir mmodal do carrinho
+ cartBtn.addEventListener("click", function() {
+    cartModal.style.display = "flex"
+ })
+
+ //Fechar modal ao clicar
+ cartModal.addEventListener("click", function(event){
+   if (event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+ })
+
+ //Botão fechar modal
+ closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+ })
+
+ menu.addEventListener("click", function(event){
+    //console.log(event.target)
+    
+    let parentButton = event.target.closest(".add-to-cart-btn")
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parentButton.getAttribute("data-price")
+        console.log(name)
+        console.log(price)
+    }   
+})
