dd2a5a7c6c3eb6549bad6e34ebb2328859a64bb1
Flávio Antônio <flavusantonio@gmail.com>
2025-07-14 23:12:24 -0300

    Corrigido base para deploy no Vercel

--- cardapio4/vite.config.js
+++ cardapio4/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),]
-})
+  base: './', // 👈 ESSA LINHA resolve o problema de 404 no deploy
+  plugins: [react(), tailwindcss()],
+});
